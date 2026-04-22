c254be45575e1cc2999c000dd4e23df9d51d0107
seetharamarajuk-bridgelabz <seetharamaraju.k@bridgelabz.com>
2026-04-22 13:03:02 +0530

    UC 6: Fleet Management (Multiple Hubs)

--- eco-ride/Eco_Ride_MainClass.py
+++ eco-ride/Eco_Ride_MainClass.py
@@ -3,6 +3,7 @@ from time import process_time
 
 from ElectricCar import ElectricCar
 from ElectricScooter import ElectricScooter
+from FleetHubManager import *
 
 from Vehicle import Vehicle
 
@@ -12,32 +13,66 @@ from Vehicle import Vehicle
 # vehicle.rental_price = 2000
 # print(vehicle)
 
-vehicle = ElectricCar("V102", "HondaI20", 10)
-vehicle.battery = 80
-vehicle.rental_price = 3000
-vehicle.maintenance_status = 'inservice'
+v1 = ElectricCar("V102", "HondaI20", 10)
+v1.battery = 80
+v1.rental_price = 3000
+v1.maintenance_status = 'inservice'
 
-print(vehicle)
+# print(v1)
 
-vehicle1 = ElectricScooter("V103", "HondaShine", 50)
-vehicle1.battery = 80
-vehicle1.rental_price = 1000
-vehicle1.maintenance_status = 'needrepair'
+v2 = ElectricScooter("V103", "HondaShine", 50)
+v2.battery = 80
+v2.rental_price = 1000
+v2.maintenance_status = 'needrepair'
 
-print(vehicle1)
+# print(v2)
 
-print(vehicle.calculate_trip_cost(20))
-print(vehicle1.calculate_trip_cost(10))
+# print(v1.calculate_trip_cost(20))
+# print(v2.calculate_trip_cost(10))
 
-print("---------------")
+# print("---------------")
 
 # Create a mixed list (The "List of Objects")
-fleet_to_process = [vehicle, vehicle1]
+# fleet_to_process = [v1, v2]
 
 # Polymorphism in action
-print("--- Processing Trip Receipts ---")
-for v in fleet_to_process:
-    # Here, 'units' is used as km for cars and minutes for scooters
-    # Python decides which formula to use at RUNTIME.
-    cost = v.calculate_trip_cost(20)
-    print(f"Vehicle {v.vehicle_id} , Vehicle Model : {v.model} , Total Cost = ${cost}")
+# print("--- Processing Trip Receipts ---")
+# for v in fleet_to_process:
+#     # Here, 'units' is used as km for cars and minutes for scooters
+#     # Python decides which formula to use at RUNTIME.
+#     cost = v.calculate_trip_cost(20)
+#     print(f"Vehicle {v.vehicle_id} , Vehicle Model : {v.model} , Total Cost = ${cost}")
+
+
+#  UC6
+
+v3 = ElectricCar("V105", "BMW", 6)
+v3.battery = 90
+v3.rental_price = 6000
+v3.maintenance_status = 'good'
+
+# print(v3)
+
+v4 = ElectricScooter("V104", "pulser220", 50)
+v4.battery = 70
+v4.rental_price = 2000
+v4.maintenance_status = 'needrepair'
+
+vehicle_list = [v1, v2, v3, v4]
+
+fleet = FleetHubManager()
+fleet.add_hub()
+fleet.add_vehicle(vehicle_list)
+print("-------------------")
+fleet.add_hub()
+fleet.add_vehicle(vehicle_list)
+print("-------------------")
+fleet.add_hub()
+fleet.add_vehicle(vehicle_list)
+print("-------------------")
+fleet.add_hub()
+fleet.add_vehicle(vehicle_list)
+
+print("-------------------")
+
+fleet.display_all_hubs()
---
+++ eco-ride/FleetHubManager.py
@@ -0,0 +1,47 @@
+from Vehicle import Vehicle
+from ElectricCar import *
+from ElectricScooter import *
+
+
+class FleetHubManager:
+
+    def __init__(self):
+        self.__hubs = {}
+
+    def add_hub(self):
+        hub = input("Enter new Hub Name (e.g., Downtown, Airport): ").strip()
+        if hub not in self.__hubs:
+            self.__hubs[hub] = []
+            print(f"Hub '{hub}' established.")
+        else:
+            print(f"hub : {hub} already Exists !!")
+
+    def display_all_hubs(self):
+        if not self.__hubs:
+            print("No Hubs registered !")
+            return
+        for hub, vehicles in self.__hubs.items():
+            print(f" Hub : {hub} Vehicles : {len(vehicles)}")
+            if not vehicles:
+                print("No vehicles registered !")
+            for v in vehicles:
+                # Polymorphism in action: calling __str__ or specific methods
+                print(v)
+
+    def add_vehicle(self, vehicle_list):
+        hub_name = input("Enter Hub Name to add vehicle to: ").strip()
+
+        if hub_name not in self.__hubs:
+            print("Error: Hub not found. Please create it first.")
+            return
+
+        v_id = input("Enter Vehicle ID to move to this hub:").strip()
+
+        # Search for the vehicle in your global inventory
+        target_vehicle = next((v for v in vehicle_list if v.vehicle_id == v_id), None)
+
+        if target_vehicle:
+            self.__hubs[hub_name].append(target_vehicle)
+            print(f"Vehicle '{target_vehicle}' added to hub '{hub_name}' !")
+        else:
+            print("Error: Vehicle ID not found in system inventory.")
